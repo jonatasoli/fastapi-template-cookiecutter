390f2580ca118e9a6be615036b5f5277e6337467
Bruno Henrique <bruno.henriquy@gmail.com>
2021-01-11 11:41:25 -0300

    Adjusting template

---
+++ {{cookiecutter.project_slug}}/app/{{cookiecutter.app_slug}}/tests/test_{{cookiecutter.app_slug|replace('-', '_')}}_endpoints.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+def test_add_{{cookiecutter.app_slug|replace('-', '_')}}():
+    ...
+
+
+def test_list_{{cookiecutter.app_slug|replace('-', '_')}}():
+    ...
+
+
+def test_list_{{cookiecutter.app_slug|replace('-', '_')}}():
+    ...
+
+
+def test_update_{{cookiecutter.app_slug|replace('-', '_')}}():
+    ...
+
+
+def test_delete_{{cookiecutter.app_slug|replace('-', '_')}}():
+    ...
